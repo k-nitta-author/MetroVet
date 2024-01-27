5153d90ebac605594eb322bd203ef513700ff83f
k-nitta-author <k.nitta.author@gmail.com>
2024-01-27 17:29:03 +0800

    added features to login screen

--- app/src/main/java/com/example/metrovetfrontend/MainActivity.java
+++ app/src/main/java/com/example/metrovetfrontend/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.metrovetfrontend;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button adminButton = (Button) findViewById(R.id.AdminButton);
+        Button userButton = (Button) findViewById(R.id.Userbutton);
+
+        adminButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+
+                i.putExtra("is_user_admin", true);
+
+                startActivity(i);
+
+            }
+        });
+
+        userButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+
+                i.putExtra("is_user_admin", false);
+
+                startActivity(i);
+            }
+        });
+
     }
+
+
 }
