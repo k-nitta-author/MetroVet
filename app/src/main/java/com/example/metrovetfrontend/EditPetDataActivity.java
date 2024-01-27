6d8e4417c38a93be428812af47e6edff11b8d7a6
k-nitta-author <k.nitta.author@gmail.com>
2024-01-27 17:04:49 +0800

    initial setup
    all screens have been created. currently without functionality.

---
+++ app/src/main/java/com/example/metrovetfrontend/EditPetDataActivity.java
@@ -0,0 +1,14 @@
+package com.example.metrovetfrontend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditPetDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_pet_data);
+    }
+}
---
+++ app/src/main/java/com/example/metrovetfrontend/SelectionScreenActivity.java
@@ -0,0 +1,14 @@
+package com.example.metrovetfrontend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectionScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selection_screen);
+    }
+}
